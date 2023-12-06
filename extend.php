5de4605cac4d7262387527b6418d122d384e3a1d
Litalino <99712477+Litalino@users.noreply.github.com>
2023-12-07 05:53:05 +0700

    up

--- extend.php
+++ extend.php
@@ -18,11 +18,11 @@ use Flarum\Api\Event\Serializing;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/resources/less/forum.less'),
+        ->css(__DIR__.'/less/forum.less'),
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/resources/less/admin.less'),
-    new Extend\Locales(__DIR__.'/resources/locale'),
+        ->css(__DIR__.'/less/admin.less'),
+    new Extend\Locales(__DIR__.'/locale'),
     
     (new Extend\Settings)
         ->serializeToForum('litalino-flarum-adlock.adBlock-div', 'litalino-flarum-adlock.adBlock-div'),
